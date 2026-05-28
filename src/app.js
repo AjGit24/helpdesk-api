2b594bb63dffb7fd1fd3b266ea145129ef3e57bc
ajaan24 <ajaannalliah911@gmail.com>
2026-05-28 11:49:21 -0400

    Day 4: JWT auth middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const authRoutes = require('./routes/auth')
+const {protect} = require('./middleware/auth');
+
 
 const app = express();
 
@@ -11,6 +13,10 @@ app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.json({message: 'Helpdesk API running'})
+});
+
+app.get('/api/protected', protect, (req, res) => {
+    res.json({message: `Hello ${req.user.name}, you are authenticated`});
 });
 
 module.exports = app;
